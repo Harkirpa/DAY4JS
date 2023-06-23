08958bf01ca0ffe76e8f163479aa56231bf9e820
Harkirpa <kirpa704@gmail.com>
2023-06-23 11:01:22 +0530

    day4

---
+++ script.js
@@ -0,0 +1,16 @@
+function Person () {
+    this.name = 'John',
+    this.age = 23,
+
+     this.greet = function () {
+        console.log('hello');
+    }
+}
+
+// create objects
+const person1 = new Person();
+const person2 = new Person();
+
+// access properties
+console.log(person1.name);  
+console.log(person2.name);
